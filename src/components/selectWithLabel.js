90137b242504c25aa0ffc69a31923f557c327157
Denis Nebytov <nebytov.d@gmail.com>
2023-06-13 17:02:09 +0300

    added selectWithLabelComponent

---
+++ src/components/selectWithLabel.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const SelectWithLabel = (props) => {
+  const {selectName, selectArray, onChangeFunction} = props
+  const selectID = selectName.split(' ').join('')
+
+  return (
+    <div className="mb-6 pr-4">
+      <label
+        htmlFor={selectID}
+        className="block mb-2 text-sm font-medium text-gray-300 focus:ring-blue-500 focus:border-blue-500"
+      >
+        {selectName}
+      </label>
+      <select
+        className="border text-sm rounded-md p-2.5 bg-gray-700 border-gray-600 text-white"
+        onChange={onChangeFunction}
+        name={selectID}
+        id={selectID}
+      >
+        {selectArray.map((arrayItem, index) => (
+          <option className="border rounded-md text-white" key={index} value={arrayItem.value}>
+            {arrayItem.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+}
+
+export default SelectWithLabel
