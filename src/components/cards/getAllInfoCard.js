bd0f75e5eadf3d563b4a1bf459762157eabe2106
Nebytov Denis <nebytov.d@gmail.com>
2024-01-10 19:07:36 +0300

    added a waiter message and error message

--- src/components/cards/getAllInfoCard.js
+++ src/components/cards/getAllInfoCard.js
@@ -11,7 +11,7 @@ import {
 } from '../../utils/helpFunctions'
 
 // we need onWaiting
-const GetAllInfoCard = ({api, wasm, setters}) => {
+const GetAllInfoCard = ({api, wasm, onWaiting, onError, setters}) => {
   const {
     setBalance,
     setAndMapUtxos,
@@ -23,56 +23,90 @@ const GetAllInfoCard = ({api, wasm, setters}) => {
     setUnregPubStakeKey,
   } = setters
   const getAllInfoClick = () => {
+    onWaiting(true)
     getBalance(api, wasm)
       .then((adaValue) => {
         console.log('[dApp][GetAllInfoCard][getBalance]: ', adaValue)
         setBalance(adaValue)
+        onWaiting(false)
       })
-      .catch((e) => console.log(e))
+      .catch((e) => {
+        console.error(e)
+        onWaiting(false)
+        onError()
+      })
 
     getUTxOs(api, wasm)
       .then((utxos) => {
         console.log('[dApp][GetAllInfoCard][getUTxOs]: ', utxos)
         setAndMapUtxos(utxos)
+        onWaiting(false)
       })
-      .catch((e) => console.log(e))
+      .catch((e) => {
+        console.error(e)
+        onWaiting(false)
+        onError()
+      })
 
     getChangeAddress(api, wasm)
       .then((bech32Addr) => {
         console.log('[dApp][GetAllInfoCard][getChangeAddress]: ', bech32Addr)
         setChangeAddress(bech32Addr)
+        onWaiting(false)
       })
-      .catch((e) => console.log(e))
+      .catch((e) => {
+        console.error(e)
+        onWaiting(false)
+        onError()
+      })
 
     getRewardAddress(api, wasm)
       .then((bech32Addr) => {
         console.log('[dApp][GetAllInfoCard][getRewardAddress]: ', bech32Addr)
         setRewardAddress(bech32Addr)
+        onWaiting(false)
       })
-      .catch((e) => console.log(e))
+      .catch((e) => {
+        console.error(e)
+        onWaiting(false)
+        onError()
+      })
 
     getPubDRepKey(api, wasm)
       .then((drepKey) => {
         console.log('[dApp][GetAllInfoCard][getPubDRepKey]: ', drepKey)
         setDRepIdBech32(drepKey.dRepIDBech32)
         setDRepIdHex(drepKey.dRepIDHex)
+        onWaiting(false)
       })
-      .catch((e) => console.log(e))
+      .catch((e) => {
+        console.error(e)
+        onWaiting(false)
+        onError()
+      })
 
     getRegPubStakeKey(api, wasm)
       .then((stakeKeyHash) => {
         console.log('[dApp][GetAllInfoCard][getRegPubStakeKey]: ', stakeKeyHash)
         setRegPubStakeKey(stakeKeyHash)
+        onWaiting(false)
       })
-      .catch((e) => console.log(e))
+      .catch((e) => {
+        console.error(e)
+        onWaiting(false)
+        onError()
+      })
 
     getUnregPubStakeKey(api, wasm)
       .then((stakeKeyHash) => {
         console.log('[dApp][GetAllInfoCard][getUnregPubStakeKey]: ', stakeKeyHash)
         setUnregPubStakeKey(stakeKeyHash)
+        onWaiting(false)
       })
       .catch((e) => {
-        console.log(e)
+        console.error(e)
+        onWaiting(false)
+        onError()
       })
   }
 
--- src/components/tabs/subtabs/cip95ToolsTab.js
+++ src/components/tabs/subtabs/cip95ToolsTab.js
@@ -9,6 +9,10 @@ import InfoPanel from './infoPanel'
 const Cip95TabTools = () => {
   const {api, connectionState} = useYoroi()
   const wasm = useWasm()
+  // Waiter label
+  const [currentWaiterState, setWaiterState] = useState(false)
+  // Error label
+  const [currentErrorState, setErrorState] = useState(false)
   // Balance
   const [currentBalance, setBalance] = useState('')
   // UTxOs
@@ -24,6 +28,11 @@ const Cip95TabTools = () => {
   const [currentRegPubStakeKey, setRegPubStakeKey] = useState('')
   // Unregistered public stake key
   const [currentUnregPubStakeKey, setUnregPubStakeKey] = useState('')
+
+  const showWarning = () =>{
+    setErrorState(true)
+    setTimeout(() => setErrorState(false), 3000)
+  }
 
   const setAndMapUtxos = (utxos) => {
     const mappedUtxos = utxos.map((utxo) => `${utxo.tx_hash} #${utxo.tx_index}: ${utxo.amount}`)
@@ -57,14 +66,20 @@ const Cip95TabTools = () => {
         <div>
           <div className="mb-2">
             {/* Get info button should be here */}
-            <GetAllInfoCard api={api} wasm={wasm} setters={setters} />
+            <GetAllInfoCard api={api} wasm={wasm} onWaiting={setWaiterState} onError={showWarning} setters={setters} />
           </div>
           <div className="mb-2">
             {/* info panel here */}
             <InfoPanel getters={getters} />
           </div>
+          <div className="grid justify-items-center content-end h-12 text-2xl">
+            <label className="text-white">{currentWaiterState ? 'Waiting ...' : ''}</label>
+            <label className="text-red-500">
+              {currentErrorState ? 'An error has happend. Please check logs.' : ''}
+            </label>
+          </div>
           <div>
-            <Cip95AdditionalPart api={api} wasm={wasm} />
+            <Cip95AdditionalPart api={api} wasm={wasm} onWaiting={setWaiterState} onError={showWarning} getters={getters} setters={setters} />
           </div>
         </div>
       ) : (
