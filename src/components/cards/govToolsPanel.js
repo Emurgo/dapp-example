734e4358069eacdcfc3d777513c391921ed626f6
Nebytov Denis <nebytov.d@gmail.com>
2024-01-24 15:26:30 +0300

    Set button name on goc tools panel

--- src/components/cards/govToolsPanel.js
+++ src/components/cards/govToolsPanel.js
@@ -2,9 +2,9 @@ import React from 'react'
 import {ModalWindowContent} from '../ui-constants'
 
 const GovToolsPanel = (props) => {
-  const {certLabel, clickFunction, children} = props
+  const {buttonName, certLabel, clickFunction, children} = props
   const handleAction = () => {
-    console.log(`[dApp][GovToolsPanel][${certLabel}] Building the certificate is called`)
+    console.log(`[dApp][GovToolsPanel][${certLabel}] Building is called`)
     // action here
     clickFunction()
   }
@@ -18,7 +18,7 @@ const GovToolsPanel = (props) => {
             className="w-full py-1 rounded-md text-xl text-white font-semibold bg-orange-700 hover:bg-orange-800 active:bg-orange-500"
             onClick={handleAction}
           >
-            Build Cert
+            {buttonName}
           </button>
         </div>
       </div>
