85f808d3bcdefe975e02baab4ecf3f320cff08e6
Denis Nebytov <nebytov.d@gmail.com>
2024-08-15 16:42:00 +0300

    using the new approach

--- src/components/tabs/subtabs/experimentalPart.js
+++ src/components/tabs/subtabs/experimentalPart.js
@@ -1,55 +1,21 @@
 import React from 'react'
-import {useCollapse} from 'react-collapsed'
 import AuthCard from '../../cards/authCard'
 import ListNFTsCard from '../../cards/listNFTsCard'
+import ExpandablePanel from '../../expandablePanel'
+import { iconCollapsed24, iconExpanded24 } from '../../ui-constants'
 
 const ExperimentalPart = ({api, onRawResponse, onResponse, onWaiting}) => {
-  const {getCollapseProps, getToggleProps, isExpanded} = useCollapse()
-
+  const panelStyles = 'border rounded bg-gray-700 border-gray-700 p-5 mt-5'
+  const panelTitleStyles = 'text-2xl'
   return (
-    <div className="border rounded bg-gray-700 border-gray-700 p-5 mt-5">
-      <div {...getToggleProps()}>
-        <div>
-          <span className="text-2xl">
-            Experimental&nbsp;
-            {isExpanded ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="inline align-text-bottom feather feather-arrow-down"
-              >
-                <line x1="12" y1="5" x2="12" y2="19"></line>
-                <polyline points="19 12 12 19 5 12"></polyline>
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="inline align-text-bottom feather feather-arrow-up"
-              >
-                <line x1="12" y1="19" x2="12" y2="5"></line>
-                <polyline points="5 12 12 5 19 12"></polyline>
-              </svg>
-            )}
-          </span>
-        </div>
-      </div>
-      <div {...getCollapseProps()}>
-        <div className="grid justify-items-stretch grid-cols-1 lg:grid-cols-3 gap-2 pt-3">
+    <ExpandablePanel
+      title="Experimental"
+      generalPanelStyles={panelStyles}
+      titleStyles={panelTitleStyles}
+      collapsedIcon={iconCollapsed24}
+      expandedIcon={iconExpanded24}
+    >
+      <div className="grid justify-items-stretch grid-cols-1 lg:grid-cols-3 gap-2 pt-3">
           <div>
             <AuthCard api={api} onRawResponse={onRawResponse} onResponse={onResponse} onWaiting={onWaiting} />
           </div>
@@ -57,8 +23,7 @@ const ExperimentalPart = ({api, onRawResponse, onResponse, onWaiting}) => {
             <ListNFTsCard api={api} onRawResponse={onRawResponse} onResponse={onResponse} onWaiting={onWaiting} />
           </div>
         </div>
-      </div>
-    </div>
+    </ExpandablePanel>
   )
 }
 
