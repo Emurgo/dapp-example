07c368ebb0c503286f248ef3472b59a74288cf52
Denis Nebytov <nebytov.d@gmail.com>
2025-05-20 15:38:00 +0300

    updated apiCardWithModal

--- src/components/cards/apiCardWithModal.js
+++ src/components/cards/apiCardWithModal.js
@@ -1,12 +1,10 @@
-import React from 'react'
 import Popup from 'reactjs-popup'
 
 export const ApiCardWithModal = (props) => {
-  const {buttonLabel, clickFunction, halfOpacity, children} = props
+  const {buttonLabel, clickFunction, halfOpacity, children, btnDisabled} = props
 
   const handleActionAndClose = (closeFunc) => {
     console.log(`[dApp][ApiCardWithModal][${buttonLabel}] is called`)
-    // action here
     clickFunction()
     closeFunc()
     console.log(`[dApp][ApiCardWithModal][${buttonLabel}] is closed`)
@@ -16,6 +14,15 @@ export const ApiCardWithModal = (props) => {
   const modal = true
   const nested = false
 
+  const contentStyle = {
+    width: '33.33%',
+    minWidth: '320px',
+    maxHeight: '90vh',
+    overflowY: 'auto',
+    border: 'none',
+    padding: '0',
+  }
+
   const buttonProps = {
     className:
       'w-full h-16 rounded-lg border-1 border-gray-600 text-white text-lg active:bg-orange-500 ' +
@@ -23,9 +30,13 @@ export const ApiCardWithModal = (props) => {
   }
 
   return (
-    <Popup trigger={<button {...buttonProps}>{buttonLabel}</button>} {...{modal, nested, overlayStyle}}>
+    <Popup 
+      trigger={<button {...buttonProps}>{buttonLabel}</button>} 
+      {...{modal, nested, overlayStyle}}
+      contentStyle={contentStyle}
+    >
       {(close) => (
-        <div className="bg-gray-900 border rounded-md border-gray-700 shadow-lg w-96 outline-none focus:outline-none">
+        <div className="bg-gray-900 border rounded-md border-gray-700 shadow-lg w-full h-full outline-none focus:outline-none flex flex-col">
           {/* close button and modal window title */}
           <div className="flex bg-blue-900">
             <div className="flex flex-auto justify-left">
@@ -43,16 +54,25 @@ export const ApiCardWithModal = (props) => {
             </div>
           </div>
           {/* content section */}
-          <div className="py-5">{children}</div>
+          <div className="py-5 flex-grow overflow-y-auto">
+            {children}
+          </div>
           {/* end of content section*/}
           {/* confirmation button */}
           <div className="flex">
             <div className="flex-auto mb-2 mt-3 mx-2">
               <button
-                className="w-full py-1 rounded-md text-xl text-white font-semibold bg-orange-700 hover:bg-orange-800 active:bg-orange-500"
+                className={`w-full py-1 rounded-md text-xl text-white font-semibold ${
+                  btnDisabled === true 
+                    ? 'bg-gray-500 cursor-not-allowed'
+                    : 'bg-orange-700 hover:bg-orange-800 active:bg-orange-500'
+                }`}
                 onClick={() => {
-                  handleActionAndClose(close)
+                  if (btnDisabled !== true) {
+                    handleActionAndClose(close)
+                  }
                 }}
+                disabled={btnDisabled === true}
               >
                 Send
               </button>
